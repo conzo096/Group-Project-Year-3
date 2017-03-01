b6968754523860dd6998c827755dc7f1e9344da4
Emmanuel Miras <prezolov@live.com>
2017-03-01 01:10:23 +0000

    More work on normal calculation compute shader.

--- Sound Visualization/Assets/Scripts/Container.cs
+++ Sound Visualization/Assets/Scripts/Container.cs
@@ -19,11 +19,12 @@ public static class NormalSolver
         return Vector3.Cross(sideAB, sideAC).normalized;
     }
     // TODO: Calculate normals on Shader
-    public static void RecalculateNormals(this Mesh mesh)
+    public static void RecalculateNormals(this Mesh mesh, bool editedMethod)
     {
         var triangles = mesh.GetTriangles(0);
         var vertices = mesh.vertices;
-        var triNormals = new Vector3[triangles.Length / 3]; //Holds the normal of each triangle
+        Vector3[] triNormals = new Vector3[triangles.Length / 3]; //Holds the normal of each triangle
+        //Debug.Log(triNormals.Length);
         var normals = new Vector3[vertices.Length];
 
         float angle = 90f * Mathf.Deg2Rad;
@@ -75,105 +76,107 @@ public static class NormalSolver
 
         // TODO: try and avoid for each
         // For each point in space
-
-        //foreach (var value in dictionary.Values)
-        //{
-        //    // For each triangle T1 that point belongs to
-        //    for (var i = 0; i < value.Count; ++i)
-        //    {
-        //        var sum = new Vector3();
-        //        // For each other triangle T2 (including self) that point belongs to
-        //        
-        //        for (var j = 0; j < value.Count; ++j)
-        //        {
-        //            // The corresponding vertex is actually the same vertex
-        //            if (value.VertexIndex[i] == value.VertexIndex[j])
-        //            {
-        //                // Add to temporary Vector3
-        //                sum += triNormals[value.TriangleIndex[j]];
-        //            }
-        //            else
-        //            {
-        //                float dot = Vector3.Dot(
-        //                    triNormals[value.TriangleIndex[i]],
-        //                    triNormals[value.TriangleIndex[j]]);
-        //                dot = Mathf.Clamp(dot, -0.99999f, 0.99999f);
-        //                float acos = Mathf.Acos(dot);
-        //                // The angle between the two triangles is less than the smoothing angle
-        //                if (acos <= angle)
-        //                {
-        //                    // Add to temporary Vector3
-        //                    sum += triNormals[value.TriangleIndex[j]];
-        //                }
-        //            }
-        //        }
-        //        // Normalize temporary Vector3 to find the average
-        //        normals[value.VertexIndex[i]] = sum.normalized;
-        //    }
-        //}
-
-        //mesh.normals = normals;
-
-        List<VertexKey> test = dictionary.Keys.ToList();
-        VertexEntry a;
-        var sum = new Vector3();
-        for (int outer = 0; outer < dictionary.Values.Count; outer++)
+        if (!editedMethod)
         {
-            a = dictionary[test[outer]];
-            int size = a.Count;
-            // For each triangle T1 that point belongs to
-            for (var i = 0; i < size; ++i)
+            foreach (var value in dictionary.Values)
             {
-                
-                // For each other triangle T2 (including self) that point belongs to
-                for (var j = 0; j < size; ++j)
+                // For each triangle T1 that point belongs to
+                for (var i = 0; i < value.Count; ++i)
                 {
-                    // The corresponding vertex is actually the same vertex
-                    if (a.VertexIndex[i] == a.VertexIndex[j])
+                    var sum = new Vector3();
+                    // For each other triangle T2 (including self) that point belongs to
+                    
+                    for (var j = 0; j < value.Count; ++j)
                     {
-                        // Add to temporary Vector3
-                        sum += triNormals[a.TriangleIndex[j]];
-                    }
-                    else
-                    {
-                        float dot = Vector3.Dot(
-                            triNormals[a.TriangleIndex[i]],
-                            triNormals[a.TriangleIndex[j]]);
-                        dot = Mathf.Clamp(dot, -0.99999f, 0.99999f);
-                        float acos = Mathf.Acos(dot);
-                        // The angle between the two triangles is less than the smoothing angle
-                        if (acos <= angle)
+                        // The corresponding vertex is actually the same vertex
+                        if (value.VertexIndex[i] == value.VertexIndex[j])
                         {
                             // Add to temporary Vector3
-                            sum += triNormals[a.TriangleIndex[j]];
+                            sum += triNormals[value.TriangleIndex[j]];
+                        }
+                        else
+                        {
+                            float dot = Vector3.Dot(
+                                triNormals[value.TriangleIndex[i]],
+                                triNormals[value.TriangleIndex[j]]);
+                            dot = Mathf.Clamp(dot, -0.99999f, 0.99999f);
+                            float acos = Mathf.Acos(dot);
+                            // The angle between the two triangles is less than the smoothing angle
+                            if (acos <= angle)
+                            {
+                                // Add to temporary Vector3
+                                sum += triNormals[value.TriangleIndex[j]];
+                            }
                         }
                     }
+                    // Normalize temporary Vector3 to find the average
+                    normals[value.VertexIndex[i]] = sum.normalized;
                 }
-                // Normalize temporary Vector3 to find the average
-                normals[a.VertexIndex[i]] = sum.normalized;
-                sum = Vector3.zero;
             }
         }
-        
+        else
+        {
+            List<VertexKey> test = dictionary.Keys.ToList();
+            VertexEntry vxEntry;
+            
+            var sum = new Vector3();
+            for (int outer = 0; outer < dictionary.Values.Count; outer++)
+            {
+                vxEntry = dictionary[test[outer]];
+                //Debug.Log(System.Runtime.InteropServices.Marshal.SizeOf(vxEntry));
+                int size = vxEntry.Count;
+                // For each triangle T1 that point belongs to
+                for (int i = 0; i < size; ++i)
+                {
+                    // For each other triangle T2 (including self) that point belongs to
+                    for (int j = 0; j < size; ++j)
+                    {
+                        // The corresponding vertex is actually the same vertex
+                        if (vxEntry.VertexIndex[i] == vxEntry.VertexIndex[j])
+                        {
+                            // Add to temporary Vector3
+                            sum += triNormals[vxEntry.TriangleIndex[j]];
+                        }
+                        else
+                        {
+                            float dot = Vector3.Dot(
+                                triNormals[vxEntry.TriangleIndex[i]],
+                                triNormals[vxEntry.TriangleIndex[j]]);
+                            dot = Mathf.Clamp(dot, -0.99999f, 0.99999f);
+                            float acos = Mathf.Acos(dot);
+                            // The angle between the two triangles is less than the smoothing angle
+                            if (acos <= angle)
+                            {
+                                // Add to temporary Vector3
+                                sum += triNormals[vxEntry.TriangleIndex[j]];
+                            }
+                        }
+                    }
+                    // Normalize temporary Vector3 to find the average
+                    normals[vxEntry.VertexIndex[i]] = sum.normalized;
+                    sum = Vector3.zero;
+                }
+            }
+        }
+        // Re-assign normals
         mesh.normals = normals;
-
 }
 
 
     private struct VertexKey
     {
-        private readonly long _x;
-        private readonly long _y;
-        private readonly long _z;
+        private readonly int _x;
+        private readonly int _y;
+        private readonly int _z;
 
         //Change this if you require a different precision.
         private const int Tolerance = 100000;
 
         public VertexKey(Vector3 position)
         {
-            _x = (long)(Mathf.Round(position.x * Tolerance));
-            _y = (long)(Mathf.Round(position.y * Tolerance));
-            _z = (long)(Mathf.Round(position.z * Tolerance));
+            _x = (int)(Mathf.Round(position.x * Tolerance));
+            _y = (int)(Mathf.Round(position.y * Tolerance));
+            _z = (int)(Mathf.Round(position.z * Tolerance));
         }
         public override bool Equals(object obj)
         {
@@ -188,30 +191,6 @@ public static class NormalSolver
 
     }
 
-    private sealed class VertexEntry
-    {
-        public int[] TriangleIndex = new int[4];
-        public int[] VertexIndex = new int[4];
-
-        private int _reserved = 4;
-        private int _count;
-
-        public int Count { get { return _count; } }
-
-        public void Add(int vertIndex, int triIndex)
-        {
-            //Auto-resize the arrays when needed
-            if (_reserved == _count)
-            {
-                _reserved *= 2;
-                Array.Resize(ref TriangleIndex, _reserved);
-                Array.Resize(ref VertexIndex, _reserved);
-            }
-            TriangleIndex[_count] = triIndex;
-            VertexIndex[_count] = vertIndex;
-            ++_count;
-        }
-    }
 }
 
 public class Container : MonoBehaviour
@@ -222,20 +201,29 @@ public class Container : MonoBehaviour
     public ComputeShader computeShader;
     public ComputeShader normalComputeShader;
     public bool calculateNormalsWithUnityBuiltIn;
+    public bool editedMethod;
 
     private CubeGrid grid;
 
     private Mesh mesh;
 
+    private ComputeBuffer vertexEntryBuffer;
+    private ComputeBuffer normalBuffer;
+    private int shaderKernel;
+
     public void Start()
     {
         this.grid = new CubeGrid(this, this.computeShader);
+        editedMethod = true;
+        calculateNormalsWithUnityBuiltIn = false;
+        // Init shader
+        //instantiateNormalComputeShader();
     }
 
     public void Update()
     {
         this.grid.evaluateAll(this.GetComponentsInChildren<MetaBall>());
-
+        
         mesh = this.GetComponent<MeshFilter>().mesh;
         mesh.Clear();
         mesh.vertices = this.grid.vertices.ToArray();
@@ -247,9 +235,43 @@ public class Container : MonoBehaviour
         }
         else
         {
-            NormalSolver.RecalculateNormals(mesh);
+            NormalSolver.RecalculateNormals(mesh, editedMethod);
+
         }
     }
 
+    private void instantiateNormalComputeShader()
+    {
+        this.shaderKernel = this.normalComputeShader.FindKernel("Calculate");
+        //this.normalBuffer = new ComputeBuffer(0, 0); // Pass in stuff
+        //this.normalBuffer.SetData(mesh.normals);
+        
+        //this.vertexEntryBuffer = new ComputeBuffer(1, sizeof(VertexEntry));
+    }
+}
 
+
+class VertexEntry
+{
+    public int[] TriangleIndex = new int[4];
+    public int[] VertexIndex = new int[4];
+
+    private int _reserved = 4;
+    private int _count;
+
+    public int Count { get { return _count; } }
+
+    public void Add(int vertIndex, int triIndex)
+    {
+        //Auto-resize the arrays when needed
+        if (_reserved == _count)
+        {
+            _reserved *= 2;
+            Array.Resize(ref TriangleIndex, _reserved);
+            Array.Resize(ref VertexIndex, _reserved);
+        }
+        TriangleIndex[_count] = triIndex;
+        VertexIndex[_count] = vertIndex;
+        ++_count;
+    }
 }
