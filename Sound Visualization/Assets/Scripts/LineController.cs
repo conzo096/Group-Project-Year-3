8f343252e48cf90b71765ca462b9e3ee19f7ac69
Conner Weatherston <40167111@live.napier.ac.uk>
2017-03-02 13:46:34 +0000

    Fixed the line.
    Still need to prevent multiple lines being drawn to the same place. Also
    need to create different colours for the line.

--- Sound Visualization/Assets/Scripts/LineController.cs
+++ Sound Visualization/Assets/Scripts/LineController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class LineController : MonoBehaviour
 {
@@ -55,6 +56,25 @@ public class LineController : MonoBehaviour
             selectedAudio = false;
             selectedMesh = false;
         }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                // Need to find out how to get parent of collider.
+                if (hit.collider.tag == "LineConnection")
+                {
+                    selectedLine = true;
+                    Debug.Log("well done, you managed to press the line. ");
+                    RemoveLine(hit.collider.gameObject);
+                    Destroy(hit.collider.gameObject, 2);     
+                }
+            }  
+        }
+
     }
 
     public void OnGUI()
@@ -76,20 +96,6 @@ public class LineController : MonoBehaviour
             }
         }
 
-       
-        foreach (GameObject obj in lineCollection)
-        {
-            if (EventSystem.current.currentSelectedGameObject == obj && selectedLine == false)
-            {
-                selectedLine = true;
-                Debug.Log("well done, you managed to press the line. ");
-
-                RemoveLine(obj);
-                
-                Destroy(obj, 2);
-                break;
-            }
-        }
     }
 
 
@@ -101,36 +107,55 @@ public class LineController : MonoBehaviour
         lineObj.layer = 5;
         lineObj.tag = "LineConnection";
         lineObj.name = "LineConnection(" + lineCounter + ")";
-        lineObj.AddComponent<Image>();
-        lineObj.AddComponent<Button>();
+   //     lineObj.AddComponent<Image>();
+  //      lineObj.AddComponent<Button>();
 
-        lineObj.transform.SetParent(GameObject.Find("Panel").transform);
+        //lineObj.transform.SetParent(GameObject.Find("Panel").transform);
 
-        RectTransform rt = lineObj.GetComponent<RectTransform>();
+   /*     RectTransform rt = lineObj.GetComponent<RectTransform>();
         rt.sizeDelta = new Vector2(1, Vector3.Distance(start, end));
         rt.anchoredPosition = new Vector2(0, 0);
         Vector3 pos = Vector3.Lerp(start, end, 0.5f);
         rt.transform.position = pos;
         rt.transform.localPosition = new Vector3(0, 0, 0);
 
-
-        // This part draws a line that cannot be interacted with, this was for testing purposes.
+    */
+        // Create visual line between two connected parameters.
         lineObj.AddComponent<LineRenderer>();
         LineRenderer lineObjRend = lineObj.GetComponent<LineRenderer>();
-        lineObjRend.enabled = false;
         lineObjRend.SetPosition(0, start);
         lineObjRend.SetPosition(1, end);
         lineCounter++;
 
+        lineObj.AddComponent<BoxCollider>();
+
+
+        // Add a collider to the line. - Need to readjust formaula. 
+        BoxCollider col = lineObj.GetComponent<BoxCollider>();
+        float lineLength = Vector3.Distance(start, end); // length of line
+        col.size = new Vector3(lineLength, 3f, 2f); // size of collider is set where X is length of line, Y is width of line, Z will be set as per requirement
+        Vector3 midPoint = (start + end) / 2;
+        col.transform.position = midPoint; // setting position of collider object
+
+        // Following lines calculate the angle between startPos and endPos
+        float angle = (Mathf.Abs(start.y - end.y) / Mathf.Abs(start.x - end.x));
+        if ((start.y < end.y && start.x > end.x) || (end.y < start.y && end.x > start.x))
+        {
+            angle *= -1;
+        }
+        angle = Mathf.Rad2Deg * Mathf.Atan(angle);
+        col.transform.Rotate(0, 0, angle);
+
+
+        // Add the line connection class to the line.
         lineObj.AddComponent<LineConnection>();
         LineConnection newLine = lineObj.GetComponent<LineConnection>();
         newLine.VisualConnection = sm.GetComponentInChildren<Text>().text;
         newLine.AudioConnection = sa.GetComponentInChildren<Text>().text;
         lineCollection.Add(lineObj);
- 
 
+        lineObj.transform.SetParent(GameObject.Find("Canvas").transform);
     }
-
 
     public void AddNewLine(string[] pair)
     {
@@ -139,7 +164,7 @@ public class LineController : MonoBehaviour
         lineCollection.Add(newLine);
     }
 
-    public  bool RemoveLine(GameObject line)
+    public bool RemoveLine(GameObject line)
     {
         if (lineCollection.Contains(line))
         {
@@ -147,7 +172,7 @@ public class LineController : MonoBehaviour
             return true;
         }
         return false;
-    }
+    } 
 
     public string FindMeshParameter(string par)
     {
