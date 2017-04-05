2c8083f3b8b669339d82321963498f429dbc5a42
Emmanuel Miras <prezolov@live.com>
2017-04-05 16:37:02 +0100

    Updated noise ring controller behaviour.

--- Sound Visualization/Assets/Scripts/NoiseRingController.cs
+++ Sound Visualization/Assets/Scripts/NoiseRingController.cs
@@ -32,13 +32,13 @@ public class NoiseRingController : MonoBehaviour {
     /* Scale */
     [Header("Scale modulators")]
     // X Axis
-    [Range(0, 1)]
+    [Range(1, 2)]
     public float xScaleModulator;
     // Y Axis
-    [Range(0, 1)]
+    [Range(1, 2)]
     public float yScaleModulator;
     // Z Axis
-    [Range(0, 1)]
+    [Range(1, 2)]
     public float zScaleModulator;
     [Range(1, 100)]
     public float scaleAmplitude;
@@ -75,7 +75,10 @@ public class NoiseRingController : MonoBehaviour {
             scaleAmplitude = 1f;
         if (rotationAmplitude == 0f)
             rotationAmplitude = 1f;
-	}
+        // Set all to 1 on init
+        childTransform[1].localScale = new Vector3(1f, 1f, 1f);
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -88,71 +91,82 @@ public class NoiseRingController : MonoBehaviour {
 
     public void UpdatePosition()
     {
-        float oscillation = 0;
+        //float oscillation = 0;
+        //
+        //switch (positionOscillationType)
+        //{
+        //    case OscType.Cos:
+        //        oscillation = (Mathf.Cos(Time.time) / positionAmplitude);
+        //        break;
+        //    case OscType.Sin:
+        //        oscillation = (Mathf.Sin(Time.time) / positionAmplitude);
+        //        break;
+        //    default:
+        //        break;
+        //}
+        //
+        //float xPosition = oscillation * xPositionModulator / 100f;
+        //float yPosition = oscillation * yPositionModulator / 100f;
+        //float zPosition = oscillation * zPositionModulator / 100f;
 
-        switch (positionOscillationType)
-        {
-            case OscType.Cos:
-                oscillation = (Mathf.Cos(Time.time) / positionAmplitude);
-                break;
-            case OscType.Sin:
-                oscillation = (Mathf.Sin(Time.time) / positionAmplitude);
-                break;
-            default:
-                break;
-        }
-
-        float xPosition = oscillation * xPositionModulator / 100f;
-        float yPosition = oscillation * yPositionModulator / 100f;
-        float zPosition = oscillation * zPositionModulator / 100f;
+        // Clamp the values
+        Mathf.Clamp(xPositionModulator, -30f, 30f);
+        Mathf.Clamp(yPositionModulator, 0f, 10f);
+        Mathf.Clamp(zPositionModulator, -25f, 25f);
 
         // Apply translation
-        transform.Translate(new Vector3(xPosition, yPosition, zPosition));
+        transform.position = (Vector3.Lerp(transform.position, new Vector3(xPositionModulator, yPositionModulator, zPositionModulator), Time.deltaTime));
     }
 
     public void UpdateScale()
     {
-        float oscillation = 0;
-
-        switch (scaleOscillationType)
-        {
-            case OscType.Cos:
-                oscillation = (Mathf.Cos(Time.time) / scaleAmplitude);
-                break;
-            case OscType.Sin:
-                oscillation = (Mathf.Sin(Time.time) / scaleAmplitude);
-                break;
-            default:
-                break;
-        }
-
-        float xScale = (oscillation * xScaleModulator) / 100f;
-        float yScale = (oscillation * yScaleModulator) / 100f;
-        float zScale = (oscillation * zScaleModulator) / 100f;
+        //float oscillation = 0;
+        //
+        //switch (scaleOscillationType)
+        //{
+        //    case OscType.Cos:
+        //        oscillation = (Mathf.Cos(Time.time) / scaleAmplitude);
+        //        break;
+        //    case OscType.Sin:
+        //        oscillation = (Mathf.Sin(Time.time) / scaleAmplitude);
+        //        break;
+        //    default:
+        //        break;
+        //}
+        //
+        //float xScale = (oscillation * xScaleModulator) / 100f;
+        //float yScale = (oscillation * yScaleModulator) / 100f;
+        //float zScale = (oscillation * zScaleModulator) / 100f;
         // Apply scale
-        transform.localScale += new Vector3(xScale, yScale, zScale);
+        // transform.localScale += new Vector3(xScale, yScale, zScale);
+
+        // childTransform[1].localScale = new Vector3(xScale, yScale, zScale);
+
+        // Apply scale to 2nd child, because Unity counts the current object as a child for some reason...
+        childTransform[1].localScale = Vector3.Lerp(childTransform[1].localScale, new Vector3(xScaleModulator, yScaleModulator, zScaleModulator), Time.deltaTime);
     }   
         
     public void UpdateRotation()
     {
-        float oscillation = 0;
-
-        switch (rotationOscillationType)
-        {
-            case OscType.Cos:
-                oscillation = (Mathf.Cos(Time.time / rotationAmplitude));
-                break;
-            case OscType.Sin:
-                oscillation = (Mathf.Sin(Time.time / rotationAmplitude));
-                break;
-            default:
-                break;
-        }
-        float xRotation = (oscillation * xRotationModulator) * Mathf.PI;
-        float yRotation = (oscillation * yRotationModulator) * Mathf.PI;
-        float zRotation = (oscillation * zRotationModulator) * Mathf.PI;
+        //float oscillation = 0;
+        //
+        //switch (rotationOscillationType)
+        //{
+        //    case OscType.Cos:
+        //        oscillation = (Mathf.Cos(Time.time / rotationAmplitude));
+        //        break;
+        //    case OscType.Sin:
+        //        oscillation = (Mathf.Sin(Time.time / rotationAmplitude));
+        //        break;
+        //    default:
+        //        break;
+        //}
+        //float xRotation = (oscillation * xRotationModulator) * Mathf.PI;
+        //float yRotation = (oscillation * yRotationModulator) * Mathf.PI;
+        //float zRotation = (oscillation * zRotationModulator) * Mathf.PI;
         // Apply rotation to 2nd child, because Unity counts the current object as a child for some reason...
-        childTransform[1].Rotate(xRotation, yRotation, zRotation);
+        //childTransform[1].Rotate(xRotationModulator, yRotationModulator, zRotationModulator);
+        childTransform[1].localEulerAngles = Vector3.Lerp(childTransform[1].localEulerAngles, new Vector3(xRotationModulator, yRotationModulator, zRotationModulator), Time.deltaTime);
     }
 
 }
